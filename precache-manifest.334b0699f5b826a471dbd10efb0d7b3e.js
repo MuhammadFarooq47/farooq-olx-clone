eaa2497dce7bf6c0818d51060e76fa8f56bca740
MuhammadFarooq47 <m.farooq.h7@gmail.com>
2020-11-13 23:47:30 -0800

    Updates

---
+++ precache-manifest.334b0699f5b826a471dbd10efb0d7b3e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3205911238821f669981b46769d3344",
+    "url": "/farooq-olx-clone/index.html"
+  },
+  {
+    "revision": "931eb3029d59b8f0e087",
+    "url": "/farooq-olx-clone/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "7793388f833470b03408",
+    "url": "/farooq-olx-clone/static/css/main.a76e3962.chunk.css"
+  },
+  {
+    "revision": "931eb3029d59b8f0e087",
+    "url": "/farooq-olx-clone/static/js/2.ba37e3f2.chunk.js"
+  },
+  {
+    "revision": "ee5006d62ce4599ab7013dec6849c0bd",
+    "url": "/farooq-olx-clone/static/js/2.ba37e3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7793388f833470b03408",
+    "url": "/farooq-olx-clone/static/js/main.54266390.chunk.js"
+  },
+  {
+    "revision": "75512ccb35248c7232e7",
+    "url": "/farooq-olx-clone/static/js/runtime-main.b44e00d2.js"
+  },
+  {
+    "revision": "38cffd8a309c80bd7e0796d10655381b",
+    "url": "/farooq-olx-clone/static/media/appstore.38cffd8a.png"
+  },
+  {
+    "revision": "5f4d942303b9e3c9e62dfabfee3386a9",
+    "url": "/farooq-olx-clone/static/media/banner.5f4d9423.PNG"
+  },
+  {
+    "revision": "deae1d9cfebbfc9e6f210e8b77577048",
+    "url": "/farooq-olx-clone/static/media/banner2.deae1d9c.jpg"
+  },
+  {
+    "revision": "f529782790b1a570abfd29ffc84a4078",
+    "url": "/farooq-olx-clone/static/media/bike.f5297827.jpg"
+  },
+  {
+    "revision": "1cec96ec9f8c57211ad8cfb376c94463",
+    "url": "/farooq-olx-clone/static/media/bmw.1cec96ec.jpg"
+  },
+  {
+    "revision": "4e538ab75bd4d05e4e6818f719af2f06",
+    "url": "/farooq-olx-clone/static/media/computer.4e538ab7.jpg"
+  },
+  {
+    "revision": "b36b33b6a29bc737cdfe6bb5d8dd898c",
+    "url": "/farooq-olx-clone/static/media/house.b36b33b6.jpg"
+  },
+  {
+    "revision": "88a3ea25e05f3e8af50472e7016dec41",
+    "url": "/farooq-olx-clone/static/media/laptop.88a3ea25.jpg"
+  },
+  {
+    "revision": "242f0e404a8ed9ba10a9a3e00f6b57dc",
+    "url": "/farooq-olx-clone/static/media/macbook.242f0e40.jpg"
+  },
+  {
+    "revision": "5be3b4d29bc8333e792a878f127279db",
+    "url": "/farooq-olx-clone/static/media/mobile.5be3b4d2.jpg"
+  },
+  {
+    "revision": "e38ed85438581162a90cfc6c6126dbc0",
+    "url": "/farooq-olx-clone/static/media/playstore.e38ed854.png"
+  },
+  {
+    "revision": "53e8437792a234a40a927e1af1a07ea6",
+    "url": "/farooq-olx-clone/static/media/shoe.53e84377.jpg"
+  },
+  {
+    "revision": "4889ae22c71902b0cc4bb0c38a9bb622",
+    "url": "/farooq-olx-clone/static/media/watch.4889ae22.jpg"
+  }
+]);
